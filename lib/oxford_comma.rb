309c3c582809c5c7f1cc90cc7c2b027b6f380b7e
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-07 11:54:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
     modified_array = array
   if modified_array.size <= 1
     modified_array.join
-  elsif modified_array.size = 2
+  elsif modified_array.size == 2
     modified_array.join(" and ")
   else
     modified_array[-1] = " and #{modified_array[-1]}"
